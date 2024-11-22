91c09c539da8d28b79833aac33606e0199f8718f
Ikubimu <ikerubidemunoz@gmail.com>
2024-11-22 14:22:47 +0100

    Empty Project QT

---
+++ build/Desktop_Qt_6_7_2-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_template-project_FILE /home/iker_ubide/UNI/QT/template-project/build/Desktop_Qt_6_7_2-Debug/template-project)
+set(__QT_DEPLOY_TARGET_template-project_TYPE EXECUTABLE)
---
+++ build/Desktop_Qt_6_7_2-Debug/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/iker_ubide/UNI/QT/template-project/build/Desktop_Qt_6_7_2-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/usr/bin/x86_64-linux-gnu-g++-13" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/home/iker_ubide/Qt/6.7.2/gcc_64/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_C_COMPILER" "/usr/bin/x86_64-linux-gnu-gcc-13" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/home/iker_ubide/Qt/6.7.2/gcc_64" CACHE "PATH" "" FORCE)
